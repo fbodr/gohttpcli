ca9a1558923238131f821d18f29cfbb7389ab5fb
Fabio Rafael da Rosa <fdr@fabiodarosa.org>
2020-02-03 13:43:52 +0100

    fixes help text

--- cmd/getToken.go
+++ cmd/getToken.go
@@ -28,13 +28,8 @@ import (
 // getTokenCmd represents the getToken command
 var getTokenCmd = &cobra.Command{
 	Use:   "getToken",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "get an authentication token from oauth provider",
+	Long:  `get an authentication token from oauth provider`,
 	Run: func(cmd *cobra.Command, args []string) {
 		access_token_url, _ := cmd.Flags().GetString("access_token_url")
 		client_id, _ := cmd.Flags().GetString("client_id")
